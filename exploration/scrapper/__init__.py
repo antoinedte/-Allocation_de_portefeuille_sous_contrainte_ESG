90f5d5b7b0b926bc46980d4d02b60eb2f9f745f4
antoinedte <102726450+antoinedte@users.noreply.github.com>
2024-02-26 10:10:00 +0100

    🧹 cleaning

--- exploration/scrapper/__init__.py
+++
@@ -1,7 +0,0 @@
-'''
-The subpackage esgscraper contains modules:
-'__main__', 'scraper', 'csrhub', 'snp_global', 'msci', 'sustainanalytics',\
-'yahoo'
-'''
-__all__ = ['scraper', 'csrhub', 'snp_global', 'msci',
-           'sustainanalytics', 'yahoo']
